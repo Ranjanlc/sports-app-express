3bfaced57c028f8beb6aa34a5ff6dd3f2fc22330
Ranjan Lamichhane <studylc29@gmail.com>
2023-08-30 05:15:00 +0545

    Added AWS access

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // ADDITIONAL-TODO: Add a prediction counter and simulate it using database and based on matchId.
 
-const express = require('express');
+const express = require("express");
 // const { graphqlHTTP } = require('express-graphql');
-const { createYoga, createSchema } = require('graphql-yoga');
-const { clearHandler } = require('graphql-http');
-const graphqlSchema = require('./graphql/schema');
+const { createYoga, createSchema } = require("graphql-yoga");
+const { clearHandler } = require("graphql-http");
+const graphqlSchema = require("./graphql/schema");
 const {
   getFootballDetails,
   getCompetitionDetails,
@@ -22,34 +22,46 @@ const {
   getBasketballMatchStats,
   getBasketballMatchLineups,
   getBasketballMatchTable,
-} = require('./graphql/resolvers');
+} = require("./graphql/resolvers");
 const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  const allowedOrigin = 'https://ballscore.vercel.app';
+  const allowedOrigin = "https://ballscore.vercel.app";
+  const allowedOrigin2 = "https://deployment.d1qzxwzcfybbvy.amplifyapp.com";
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   // Check if the request origin matches the allowed origin
   if (req.headers.origin === allowedOrigin) {
-    res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+    res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
     res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, PATCH, DELETE'
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE"
     );
     res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization'
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
     );
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);
-    }
+
     next();
+  } else if (req.headers.origin === allowedOrigin2) {
+    res.setHeader("Access-Control-Allow-Origin", allowedOrigin2);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
   } else {
-    return res.status(403).send('FORBIDDEN REQUEST BRO');
+    return res.status(403).send("FORBIDDEN REQUEST BRO");
   }
 });
 
 app.use(
-  '/graphql',
+  "/graphql",
   createYoga({
     schema: createSchema({
       typeDefs: graphqlSchema,
